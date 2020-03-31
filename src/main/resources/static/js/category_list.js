dd6579fd8110efe4621719024406d25d50d4ad5a
mayumi-ohno <mayumi.ono@rakus-partners.co.jp>
2020-03-31 10:34:21 +0900

    商品一覧上のカテゴリリンクを押下した際、カテゴリリストが適正に生成されないバグを修正

--- src/main/resources/static/js/category_list.js
+++ src/main/resources/static/js/category_list.js
@@ -1,3 +1,4 @@
+// 参考URL: https://try-m.co.jp/blog/web-create/936/
 $(function() {
 	
 	var $parentCatogories =$("select[name='parentCategoryId']"); // 親カテゴリの要素を変数化
@@ -5,23 +6,39 @@ $(function() {
 	var $grandChildCtegories = $("select[name='grandChildCategoryId']"); // 孫カテゴリの要素を変数化
 	var originalChildCategories = $childCtegories.html(); // 後のイベントで、不要なoption要素を削除するため、オリジナルをとっておく
 	var originalGrandChildCategories = $grandChildCtegories.html(); // 同上
-
-	// 親カテゴリ未選択の場合、子・孫カテゴリをdisabledにする
+	
 	changeStatusOfChildCategories(); 
 	changeStatusOfGrandChildCategories();
+
+	// 親カテゴリ未選択の場合：子・孫カテゴリも未選択にする
+	// 親カテゴリ選択済の場合：子カテゴリリストを編集
 	if ($parentCatogories.val() == "") {
 		$grandChildCtegories.val("");
 		$childCtegories.val("");
-	}
-	if ($childCtegories.val() == "") {
-		$grandChildCtegories.val("");
+	}else{
+		modifyChildCategories();
 	}
 	
-	// -------親カテゴリ変更時------------------------------------------------------------------------------------
+	// 子カテゴリ未選択の場合：孫カテゴリも未選択にする
+	// 子カテゴリ選択済の場合：孫カテゴリリストを編集
+	if ($childCtegories.val() == "") {
+		$grandChildCtegories.val("");
+	}else{
+		modifyGrandChildCategories();
+	}
+	
 	$parentCatogories.change(function() {
-
+		modifyChildCategories();
+	});
+	
+	$childCtegories.change(function() {
+		modifyGrandChildCategories();
+	});
+		
+	// -------子カテゴリリスト編集メソッド-----------------------------------------------
+	function modifyChildCategories() {		
 		// 選択された親カテゴリのvalueを取得し変数化
-		var parentSelected = $(this).val();
+		var parentSelected = $parentCatogories.val();
 
 		// 削除された要素をもとに戻すため.html(original)を入れておく
 		$childCtegories.html(originalChildCategories).find('option').each(function() {
@@ -35,16 +52,17 @@ $(function() {
 		});
 
 		changeStatusOfChildCategories();
-		$grandChildCtegories.attr('disabled', 'disabled');
-		$childCtegories.val("");
-		$grandChildCtegories.val("");
-});
+		changeStatusOfGrandChildCategories();
+		if ($parentCatogories.val() == "") {
+			$grandChildCtegories.val("");
+			$childCtegories.val("");
+		}
+	}
 	
-	// -------子カテゴリ変更時------------------------------------------------------------------------------------
-	$childCtegories.change(function() {
-		
+	// -------孫カテゴリリスト編集メソッド-----------------------------------------------
+	function modifyGrandChildCategories() {
 		// 選択された子カテゴリのvalueを取得し変数化
-		var childSelected = $(this).val();
+		var childSelected = $childCtegories.val();
 		
 		// 削除された要素をもとに戻すため.html(original)を入れておく
 		$grandChildCtegories.html(originalGrandChildCategories).find('option').each(function() {
@@ -58,9 +76,11 @@ $(function() {
 		});
 		
 		changeStatusOfGrandChildCategories();
-		$grandChildCtegories.val("");
-	});
-		
+		if ($childCtegories.val() == "") {
+			$grandChildCtegories.val("");
+		}
+	}
+	
 		// -------子・孫カテゴリセレクトボックスの有効/無効切り替えメソッド-----------------------------------------------
 		function changeStatusOfChildCategories() {
 			if ($parentCatogories.val() == "") {
@@ -77,5 +97,3 @@ $(function() {
 			}
 		}
 });
-
-// 参考URL: https://try-m.co.jp/blog/web-create/936/
